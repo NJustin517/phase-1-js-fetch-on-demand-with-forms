4793ef1d3d7109ea48d079f46b1edb10190e1b48
Justin Luciano <justinlluciano@gmail.com>
2021-12-21 09:55:05 -0800

    commit tree 6d5c0505c3ab304105de347ea4dc51448ded5d96

--- src/index.js
+++ src/index.js
@@ -1,5 +1,19 @@
 const init = () => {
-  
-}
+  const inputForm = document.querySelector("form");
+  inputForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const input = e.target.searchByID.value;
+    fetch(`http://localhost:3000/movies/${input}`)
+      .then((response) => response.json())
+      .then((data) => {
+        const title = document.querySelector("#movieDetails h4");
+        const summary = document.querySelector("#movieDetails p");
+        title.textContent = data.title;
+        summary.textContent = data.summary;
+        console.log(title);
+      });
+    inputForm.reset();
+  });
+};
 
-document.addEventListener('DOMContentLoaded', init);
+document.addEventListener("DOMContentLoaded", init);
